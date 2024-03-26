01de72fb5ff6fd52bce871c24000d50806acc17a
Fatoye Oluwaseun <fatoyeoluwaseun@gmail.com>
2024-03-26 10:44:37 +0100

    added the 'complete tab'

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,18 @@ type TodoItem = {
   description: string;
 };
 
+type filteredItem = {
+  completedOn: string;
+  title: string;
+  description: string;
+};
+
 export default function Home() {
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
   const [allTodos, setAllTodos] = useState<TodoItem[]>([]);
   const [newTitle, setNewTitle] = useState("");
   const [newDescription, setNewDescription] = useState("");
+  const [completedTodos, setCompletedTodos] = useState<filteredItem[]>([]);
 
   const handleAddTodo = () => {
     let newTodoItem: TodoItem = {
@@ -32,6 +39,28 @@ export default function Home() {
 
     localStorage.setItem("todolist", JSON.stringify(reducedTodo));
     setAllTodos(reducedTodo);
+  };
+
+  const handleComplete = (index: number) => {
+    let now = new Date();
+    let dd = now.getDate();
+    let mm = now.getMonth() + 1;
+    let yyyy = now.getFullYear();
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+    let completedOn = `${dd} - ${mm} - ${yyyy} at ${h} : ${m} : ${s}`;
+
+    let filteredItem = {
+      ...allTodos[index],
+      completedOn: completedOn,
+    };
+
+    // let updatedCompletedArr = [...completedTodos];
+    let updatedCompletedArr: filteredItem[] = [...completedTodos];
+    updatedCompletedArr.push(filteredItem);
+    setCompletedTodos(updatedCompletedArr);
+    handleDeleteTodo(index);
   };
 
   useEffect(() => {
@@ -112,33 +141,65 @@ export default function Home() {
 
           {/* todo list */}
           <div className="todo-list">
-            {allTodos.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className="todo-list-item bg-gray-900 p-6 px-3 mt-4 flex justify-between items-center "
-                >
-                  <div className="shadow-4xl text-gray-500 flex flex-col mb-3">
-                    <h3 className="font-bold text-2xl text-green-600">
-                      {item.title}
-                    </h3>
-                    <p>{item.description}</p>
-                  </div>
+            {isCompleteScreen === false &&
+              allTodos.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="todo-list-item bg-gray-900 p-6 px-3 mt-4 flex justify-between items-center "
+                  >
+                    <div className="shadow-4xl text-gray-500 flex flex-col mb-3">
+                      <h3 className="font-bold text-2xl text-green-600">
+                        {item.title}
+                      </h3>
+                      <p>{item.description}</p>
+                    </div>
 
-                  <div className="icons flex gap-2 text-4xl cursor-pointer">
-                    <AiOutlineDelete
-                      onClick={() => handleDeleteTodo(index)}
-                      title="Delete?"
-                      className="delete-icon text-white hover:text-red-600"
-                    />
-                    <FaCheck
-                      title="Complete?"
-                      className="check-icon text-green-600 hover:text-green-800"
-                    />
+                    <div className="icons flex gap-2 text-4xl cursor-pointer">
+                      <AiOutlineDelete
+                        onClick={() => handleDeleteTodo(index)}
+                        title="Delete?"
+                        className="delete-icon text-white hover:text-red-600"
+                      />
+                      <FaCheck
+                        onClick={() => {
+                          handleComplete(index);
+                        }}
+                        title="Complete?"
+                        className="check-icon text-green-600 hover:text-green-800"
+                      />
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+
+            {isCompleteScreen === true &&
+              completedTodos.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="todo-list-item bg-gray-900 p-6 px-3 mt-4 flex justify-between items-center "
+                  >
+                    <div className="shadow-4xl text-gray-500 flex flex-col mb-3">
+                      <h3 className="font-bold text-2xl text-green-600">
+                        {item.title}
+                      </h3>
+                      <p>{item.description}</p>
+                      <p>
+                        <small>Completed on: {item.completedOn}</small>
+                      </p>
+                    </div>
+
+                    <div className="icons flex gap-2 text-4xl cursor-pointer">
+                      <AiOutlineDelete
+                        onClick={() => handleDeleteTodo(index)}
+                        title="Delete?"
+                        className="delete-icon text-white hover:text-red-600"
+                      />
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
